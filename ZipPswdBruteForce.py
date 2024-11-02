91701d7aa8c90bed132951ed81388e6c08d2b061
null <68474200+nullsleeps@users.noreply.github.com>
2024-11-02 10:21:39 -0400

    Update and rename main.py to ZipPswdBruteForce.py

---
+++ ZipPswdBruteForce.py
@@ -0,0 +1,29 @@
+import zipfile
+import itertools
+
+charlist = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[{]}";.,/`~±§'
+max_length = 4 
+
+# Open the ZIP file
+z = zipfile.ZipFile('secret.zip')
+tries = 0
+
+def try_password(z, password):
+    try:
+        z.extractall(pwd=password.encode('ascii'))
+        print(f'Password found after {tries} tries: {password}')
+        return True
+    except (RuntimeError, zipfile.BadZipFile):
+        return False
+
+for length in range(1, max_length + 1):
+    for password in itertools.product(charlist, repeat=length):
+        tries += 1
+        password = ''.join(password)
+        if try_password(z, password):
+            break
+    else:
+        continue
+    break
+else:
+    print("Password not found.")
